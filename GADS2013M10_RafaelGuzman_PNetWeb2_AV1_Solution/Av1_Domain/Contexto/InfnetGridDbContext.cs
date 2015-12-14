bf4223651f7e8f31612eac95502c65a40fe9e18f
rqguzman <rafael.guzman@outlook.com>
2015-12-14 07:46:54 -0200

    Added Entity Framework

--- GADS2013M10_RafaelGuzman_PNetWeb2_AV1_Solution/Av1_Domain/Contexto/InfnetGridDbContext.cs
+++ GADS2013M10_RafaelGuzman_PNetWeb2_AV1_Solution/Av1_Domain/Contexto/InfnetGridDbContext.cs
@@ -1,13 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity;
+using Av1_Domain.Entidades;
 
 namespace Av1_Domain.Contexto
 {
-    public class InfnetGridDbContext
+    public class InfnetGridDbContext : DbContext
     {
+        public DbSet<CanalDeTv> CanaisDeTv { get; set; }
+
+        public DbSet<Episodio> Episodios { get; set; }
+
+        public DbSet<GeneroDaSerie> GenerosDasSeries { get; set; }
+
+        public DbSet<PaisDeOrigemSerie> PaisesDeOrigemSeries { get; set; }
+
+        public DbSet<Serie> Series { get; set; }
+
+        public DbSet<Temporada> Temporadas { get; set; }
+
+        public DbSet<TipoDeArquivo> TiposDeArquivo { get; set; }
 
     }
 }
